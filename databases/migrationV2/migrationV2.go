09dd9cc986028c8088bedcc070f3f35eb52df11e
Supakornn <supakorn0728@gmail.com>
2025-01-15 18:15:40 +0700

    :sparkles: migrate item to database

---
+++ databases/migrationV2/migrationV2.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"github.com/supakorn/game-shop/config"
+	"github.com/supakorn/game-shop/databases"
+	"github.com/supakorn/game-shop/entities"
+	"gorm.io/gorm"
+)
+
+func main() {
+	conf := config.ConfigGetting()
+	db := databases.NewPostgresDB(conf.Database)
+
+	tx := db.ConnectionGetting().Begin()
+
+	itemsAdding(tx)
+
+	if err := tx.Commit().Error; err != nil {
+		tx.Rollback()
+		panic(err)
+	}
+}
+
+func itemsAdding(tx *gorm.DB) {
+	items := []entities.Item{
+		{
+			Name:        "Sword",
+			Description: "A sword is a bladed melee weapon.",
+			Price:       100,
+			Picture:     "https://example.com/sword.jpg",
+		},
+		{
+			Name:        "Shield",
+			Description: "A shield is a piece of personal armour held in the hand.",
+			Price:       50,
+			Picture:     "https://example.com/shield.jpg",
+		},
+		{
+			Name:        "Bow",
+			Description: "A bow is a flexible.",
+			Price:       200,
+			Picture:     "https://example.com/bow.jpg",
+		},
+		{
+			Name:        "Arrow",
+			Description: "An arrow is a fin-stabilized projectile that is launched via a bow",
+			Price:       10,
+			Picture:     "https://example.com/arrow.jpg",
+		},
+	}
+
+	tx.CreateInBatches(items, len(items))
+}
