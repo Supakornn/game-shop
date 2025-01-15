753941800133b50682d5b62fec135c95c79fb1c2
Supakornn <supakorn0728@gmail.com>
2025-01-15 10:05:54 +0700

    :sparkles: start echo server && gracefull shutdown

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"github.com/supakorn/game-shop/config"
+	"github.com/supakorn/game-shop/databases"
+	"github.com/supakorn/game-shop/server"
+)
 
+func main() {
+	conf := config.ConfigGetting()
+	db := databases.NewPostgresDB(conf.Database)
+	server := server.NewEchoServer(conf, db.ConnectionGetting())
+
+	server.Start()
 }
--- server/server.go
+++ server/server.go
@@ -1,1 +1,75 @@
 package server
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/gommon/log"
+	"github.com/supakorn/game-shop/config"
+	"gorm.io/gorm"
+)
+
+type echoServer struct {
+	app  *echo.Echo
+	db   *gorm.DB
+	conf *config.Config
+}
+
+var (
+	once   sync.Once
+	server *echoServer
+)
+
+func NewEchoServer(conf *config.Config, db *gorm.DB) *echoServer {
+	echoApp := echo.New()
+	echoApp.Logger.SetLevel(log.DEBUG)
+
+	once.Do(func() {
+		server = &echoServer{
+			app:  echoApp,
+			db:   db,
+			conf: conf,
+		}
+	})
+
+	return server
+}
+
+func (s *echoServer) Start() {
+	s.app.GET("/v1/health", s.healthCheck)
+
+	quitCh := make(chan os.Signal, 1)
+	signal.Notify(quitCh, syscall.SIGINT, syscall.SIGTERM)
+	go s.gracefullShutdown(quitCh)
+
+	s.httpListening()
+}
+
+func (s *echoServer) httpListening() {
+	url := fmt.Sprintf(":%d", s.conf.Server.Port)
+
+	if err := s.app.Start(url); err != nil && err != http.ErrServerClosed {
+		s.app.Logger.Fatal("Error: ", err.Error())
+	}
+}
+
+func (s *echoServer) gracefullShutdown(quitCh chan os.Signal) {
+	ctx := context.Background()
+
+	<-quitCh
+	s.app.Logger.Info("Shutting down server...")
+
+	if err := s.app.Shutdown(ctx); err != nil {
+		s.app.Logger.Fatal("Error: ", err.Error())
+	}
+}
+
+func (s *echoServer) healthCheck(c echo.Context) error {
+	return c.String(http.StatusOK, "OK")
+}
