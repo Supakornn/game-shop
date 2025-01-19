b7f98dc3d14e3be7f864c719014d333264ab7a71
Supakornn <supakorn0728@gmail.com>
2025-01-19 23:27:02 +0700

    :sparkles: paginate itemList response

--- pkg/itemShop/service/itemShopServiceImpl.go
+++ pkg/itemShop/service/itemShopServiceImpl.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"github.com/supakorn/game-shop/entities"
 	_itemShopModel "github.com/supakorn/game-shop/pkg/itemShop/model"
 	_itemShopRepository "github.com/supakorn/game-shop/pkg/itemShop/repository"
 )
@@ -13,16 +14,42 @@ func NewItemShopServiceImpl(itemShopRepository _itemShopRepository.ItemShopRepos
 	return &itemShopServiceImpl{itemShopRepository: itemShopRepository}
 }
 
-func (s *itemShopServiceImpl) Listing(itemFilter *_itemShopModel.ItemFilter) ([]*_itemShopModel.Item, error) {
+func (s *itemShopServiceImpl) Listing(itemFilter *_itemShopModel.ItemFilter) (*_itemShopModel.ItemResult, error) {
 	itemList, err := s.itemShopRepository.Listing(itemFilter)
 	if err != nil {
 		return nil, err
 	}
 
+	itemCounting, err := s.itemShopRepository.Counting(itemFilter)
+	if err != nil {
+		return nil, err
+	}
+
+	totalPage := s.totalPageCalculation(itemCounting, itemFilter.Size)
+
+	return s.toItemResultRes(itemList, itemFilter.Page, totalPage), nil
+}
+
+func (s *itemShopServiceImpl) totalPageCalculation(totalItem int64, Size int64) int64 {
+	totalPage := totalItem / Size
+	if totalItem%Size != 0 {
+		totalPage++
+	}
+
+	return totalPage
+}
+
+func (s *itemShopServiceImpl) toItemResultRes(itemEntityList []*entities.Item, page, totalPage int64) *_itemShopModel.ItemResult {
 	itemModelList := make([]*_itemShopModel.Item, 0)
-	for _, item := range itemList {
+	for _, item := range itemEntityList {
 		itemModelList = append(itemModelList, item.ToItemModel())
 	}
 
-	return itemModelList, nil
+	return &_itemShopModel.ItemResult{
+		Items: itemModelList,
+		Paginate: _itemShopModel.PaginateResult{
+			Page:      page,
+			TotalPage: totalPage,
+		},
+	}
 }
